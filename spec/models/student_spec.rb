8d5433d46d6881d45271f370ef26c6803650a8bb
rickie <rickiemaweu@gmail.com>
2022-09-30 12:08:09 +0300

    done

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,29 +1,49 @@
 require 'rails_helper'
 
-RSpec.describe Student, type: :model do
-  let!(:student) { Student.new(first_name: 'Dwayne', last_name: 'Johnson', grade: 99) }
+RSpec.describe "Students", type: :request do
+  before do
+    Student.create(first_name: 'Dwayne', last_name: 'Johnson', grade: 99)
+    Student.create(first_name: 'Idris', last_name: 'Elba', grade: 105)
+    Student.create(first_name: 'Vanessa', last_name: 'Kirby', grade: 85)
+    Student.create(first_name: 'Jason', last_name: 'Statham', grade: 70)
+  end
 
-  describe '#first_name' do
-    it 'returns the student\'s first name' do
-      expect(student.first_name).to eq('Dwayne')
+  describe "GET /students" do
+    it 'returns an array of all students' do
+      get '/students'
+
+      expect(response.body).to include_json([
+        { first_name: 'Dwayne', last_name: 'Johnson', grade: 99 },
+        { first_name: 'Idris', last_name: 'Elba', grade: 105 },
+        { first_name: 'Vanessa', last_name: 'Kirby', grade: 85 },
+        { first_name: 'Jason', last_name: 'Statham', grade: 70 }
+      ])
     end
   end
 
-  describe '#last_name' do
-    it 'returns the student\'s last name' do
-      expect(student.last_name).to eq('Johnson')
-    end
-  end
+  describe "GET /students/grades" do
+    it 'returns an array of all students ordered by grade' do
+      get '/students/grades'
 
-  describe '#grade' do
-    it 'returns the student\'s grade' do
-      expect(student.grade).to eq(99)
+      expect(response.body).to include_json([
+        { first_name: 'Idris', last_name: 'Elba', grade: 105 },
+        { first_name: 'Dwayne', last_name: 'Johnson', grade: 99 },
+        { first_name: 'Vanessa', last_name: 'Kirby', grade: 85 },
+        { first_name: 'Jason', last_name: 'Statham', grade: 70 }
+      ])
     end
   end
+  
+  ## BONUS: un-comment out the code below to run the bonus test
 
-  describe '#to_s' do
-    it 'has a #to_s method that returns the student\'s full name' do
-      expect(student.to_s).to eq('Dwayne Johnson')
+  describe "GET /students/highest-grade" do
+    it 'returns the student with the highest grade' do
+      get '/students/highest-grade'
+
+      expect(response.body).to include_json({ 
+        first_name: 'Idris', last_name: 'Elba', grade: 105 
+      })
     end
   end
+  
 end
